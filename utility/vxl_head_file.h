33776a7811bbcfcebc4e801848a86c676fcf0b90
jimmy <jimmy@vpn23.cs.ubc.ca>
2015-11-29 15:47:21 -0800

    add utility

---
+++ utility/vxl_head_file.h
@@ -0,0 +1,37 @@
+//
+//  vxl_head_file.h
+//  OnlineStereo
+//
+//  Created by jimmy on 9/12/15.
+//  Copyright (c) 2015 Nowhere Planet. All rights reserved.
+//
+
+#ifndef OnlineStereo_vxl_head_file_h
+#define OnlineStereo_vxl_head_file_h
+
+//
+#include <vil/vil_image_view.h>
+#include <vil/vil_crop.h>
+
+#include <vpgl/vpgl_perspective_camera.h>
+
+#include <vgl/vgl_line_2d.h>
+#include <vgl/vgl_point_2d.h>
+#include <vgl/vgl_intersection.h>
+#include <vgl/vgl_distance.h>
+
+#include <vnl/vnl_random.h>
+#include <vnl/vnl_inverse.h>
+#include <vnl/vnl_matlab_filewrite.h>
+
+#include <vcl_vector.h>
+#include <vcl_algorithm.h>
+#include <vcl_utility.h>
+#include <vcl_limits.h>
+
+#include <vil/vil_load.h>
+#include <vil/vil_save.h>
+
+
+
+#endif
